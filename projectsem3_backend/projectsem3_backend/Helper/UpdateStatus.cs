af9b9ff543839e376fefc65875ad83e60aabd808
tran-thien-nhan <pipclupnomad@gmail.com>
2024-01-18 15:18:00 +0700

    update status in helper folder

--- projectsem3_backend/projectsem3_backend/Helper/UpdateStatus.cs
+++ projectsem3_backend/projectsem3_backend/Helper/UpdateStatus.cs
@@ -1,30 +1,31 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.EntityFrameworkCore;
 using projectsem3_backend.data;
-using System.Threading.Tasks; 
+using System.Threading.Tasks;
 
 namespace projectsem3_backend.Helper
 {
     public class UpdateStatus<T> where T : class, IVisibleEntity
     {
-        private readonly DatabaseContext db;
+        private readonly DatabaseContext _db;
 
         public UpdateStatus(DatabaseContext db)
         {
-            this.db = db;
+            _db = db;
         }
 
         public async Task<T> UpdateStatusObject(int id)
         {
-            var entity = await db.Set<T>().FindAsync(id);
+            var entity = await _db.Set<T>().FindAsync(id);
+
             if (entity != null)
             {
                 entity.Visible = !entity.Visible;
-                db.Set<T>().Update(entity);
-                await db.SaveChangesAsync();
+                _db.Set<T>().Update(entity);
+                await _db.SaveChangesAsync();
             }
+
             return entity;
         }
     }
