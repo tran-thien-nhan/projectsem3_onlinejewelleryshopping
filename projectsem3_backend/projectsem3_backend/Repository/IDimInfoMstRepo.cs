f44bc78fd408c464d7c0446c440c1f05563c8db3
thehungdev <thehung12th@gmail.com>
2024-02-17 09:25:03 +0700

    DimInfo Hung

---
+++ projectsem3_backend/projectsem3_backend/Repository/IDimInfoMstRepo.cs
@@ -0,0 +1,15 @@
+﻿using projectsem3_backend.CustomStatusCode;
+using projectsem3_backend.Models;
+
+namespace projectsem3_backend.Repository
+    {
+    public interface IDimInfoMstRepo
+        {
+        Task<CustomResult> GetAllDimInfoMsts();
+        Task<CustomResult> GetDimInfoMstById( string id );
+        Task<CustomResult> CreateDimInfoMst( DimInfoMst dimInfoMst, IFormFile file );
+        Task<CustomResult> UpdateDimInfoMst( DimInfoMst dimInfoMst, IFormFile file );
+        Task<CustomResult> DeleteDimInfoMst( string id );
+        Task<CustomResult> UpdateVisibility( string id );
+        }
+    }
